eb1c49d2e1919ec3a26b6bf16705815b1167c21b
LeThinhPhat <lethinhphat7102003@gmail.com>
2025-08-19 16:07:19 +0700

    Build Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600 underline hover:text-red-500 transition duration-500">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
