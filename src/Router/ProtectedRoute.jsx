e192713dd789dfcfda641719b9d2b7674ad0c802
LeThinhPhat <lethinhphat7102003@gmail.com>
2025-08-21 15:43:18 +0700

    update

--- src/Router/ProtectedRoute.jsx
+++ src/Router/ProtectedRoute.jsx
@@ -8,33 +8,3 @@ const ProtectedRoute = ({ children }) => {
 };
 
 export default ProtectedRoute;
-
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import SignIn from "./Page/SignIn";
-// import SignUp from "./Page/SignUp";
-// import ForgotPassword from "./Page/ForgotPassword";
-// import { Toaster } from "react-hot-toast";
-// // Trang Home mẫu
-// const Home = () => (
-//   <h1 style={{ textAlign: "center", marginTop: "50px" }}>
-//     Welcome to Home Page 🎉
-//   </h1>
-// );
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/signin" element={<SignIn />} />
-//         <Route path="/signup" element={<SignUp />} />
-//         <Route path="/forgot-password" element={<ForgotPassword />} />
-//         <Route path="/home" element={<Home />} /> {/* Trang sau login */}
-//         <Route path="/" element={<SignIn />} /> {/* Trang mặc định */}
-//       </Routes>
-//       <Toaster />
-//     </Router>
-//   );
-// };
-
-// export default App;  bạn có thể tách compontend ra giống code wor treen được ko
