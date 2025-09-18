f9024e3c8d2e51b9db56a1e20fc9c5357a78ebe5
LeThinhPhat <lethinhphat7102003@gmail.com>
2025-09-18 16:34:37 +0700

    ui

--- src/components/PaymentOrder/OrderPending.jsx
+++ src/components/PaymentOrder/OrderPending.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import toast from "react-hot-toast";
-import confirmPaymentService from "../../Services/SharedService/confirmPaymentService";
+import confirmPaymentService from "../../Services/Payment/confirmPaymentService";
 
 const OrderPending = ({
   orders,
