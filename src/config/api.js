52fa7033022cabdb485b3e131279d20ce7b5a0c7
Van Quoc Thinh <thinhvan.231003@gmail.com>
2025-11-06 16:40:44 +0700

    update orderList

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API_BASE_URL = 
-import.meta.env.VITE_API_BASE_URL || "https://t-6cn5.onrender.com/api";
+//import.meta.env.VITE_API_BASE_URL || "https://t-6cn5.onrender.com/api";
+import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: { "Content-Type": "application/json" },
